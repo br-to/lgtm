7037dcf865710947fcfce9f7d558e6659017e0e7
br-to <barabara0224@gmail.com>
2024-10-20 01:57:08 +0900

    testコンポ削除

--- src/components/Test.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react";
-import styles from "./Test.module.css";
-
-const Test: FC = () => {
-	return <h2 className={styles["test-title"]}>title</h2>;
-};
-
-export default Test;
