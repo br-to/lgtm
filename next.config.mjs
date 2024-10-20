bba21d8d93d2f47c995019d10f4403a656dd4384
br-to <barabara0224@gmail.com>
2024-10-21 00:45:05 +0900

    一旦ドメイン許可

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // TODO: 後で消す
+    domains: [
+      'lgtm-images.lgtmeow.com',
+      ],
+    },
+};
 
 export default nextConfig;
