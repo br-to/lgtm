4ece7e0f0d09cd366b52446eda9cec0ea62d8e84
br-to <barabara0224@gmail.com>
2024-10-21 23:01:53 +0900

    supabaseの定数管理

---
+++ src/constants/supabase.ts
@@ -0,0 +1,6 @@
+export const SUPABASE = {
+	IMAGE_URL:
+		"https://lojryqlxdiznqdqzsdiw.supabase.co/storage/v1/object/public/br-to-images/images/",
+	BUCKET_NAME: "br-to-images",
+	DIRECTORY: "images",
+} as const;
