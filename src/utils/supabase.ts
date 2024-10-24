a4a4a0ec4cf59c8e1dfc3068123374ec6400e91d
br-to <barabara0224@gmail.com>
2024-10-25 01:55:25 +0900

    upload関数の追加

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,10 +1,11 @@
 import { SUPABASE } from "@/constants/supabase";
 import { createClient } from "@supabase/supabase-js";
+import { v4 as uuidv4 } from "uuid";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {});
 
 // 画像の取得
 export const fetchImages = async () => {
@@ -19,5 +20,25 @@ export const fetchImages = async () => {
 	return {
 		imagesData: data,
 		error,
+	};
+};
+
+// 画像のアップロード
+export const uploadImage = async (file: File) => {
+	const { data, error } = await supabase.storage
+		.from(SUPABASE.BUCKET_NAME)
+		.upload(
+			`${SUPABASE.DIRECTORY}/${uuidv4()}.${file.name?.split(".").at(-1)}`,
+			file,
+		);
+
+	// TODO: エラーハンドリング
+	if (error) {
+		console.error(error);
+		return;
+	}
+
+	return {
+		uploadImageData: data,
 	};
 };
