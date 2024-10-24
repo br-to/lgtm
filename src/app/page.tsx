e2ec07221d31909aa982251ae75b309bd4d44493
br-to <barabara0224@gmail.com>
2024-10-25 01:55:43 +0900

    ファイルアップロードできるまで

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,38 @@
 import SectionTopPage from "@/components/app/top/sections/SectionTopPage";
 import Footer from "@/components/layouts/Footer";
 import Header from "@/components/layouts/Header";
+import { SUPABASE } from "@/constants/supabase";
+import { fetchImages } from "@/utils/supabase";
 import styles from "./page.module.css";
 
-export default function Home() {
+const Top = async () => {
+	const { imagesData, error } = await fetchImages();
+
+	// TODO: エラーハンドリング
+	if (error) {
+		return null;
+	}
+
+	const images =
+		(imagesData &&
+			imagesData.length > 0 &&
+			imagesData
+				.filter(({ name }) => name !== ".emptyFolderPlaceholder")
+				.map(({ name }) => ({
+					url: `${SUPABASE.IMAGE_URL}${name}`,
+					alt: name.split(".")[0],
+				}))) ||
+		undefined;
+
 	return (
 		<div className={styles["top-page"]}>
 			<Header />
 			<main className={styles["main"]}>
-				<SectionTopPage />
+				<SectionTopPage images={images} />
 			</main>
 			<Footer />
 		</div>
 	);
-}
+};
+
+export default Top;
