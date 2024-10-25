4909ff9492a83715a691740cce3486f0685dda94
br-to <barabara0224@gmail.com>
2024-10-26 00:47:32 +0900

    修正

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,11 +1,11 @@
 import { SUPABASE } from "@/constants/supabase";
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 import { v4 as uuidv4 } from "uuid";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {});
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey, {});
 
 // 画像の取得
 export const fetchImages = async () => {
