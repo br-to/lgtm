9229b4b7405653925ca59a63d041234e6ff51367
br-to <barabara0224@gmail.com>
2024-10-26 11:18:35 +0900

    アクセシビリティをよくするためbuttontタグで囲う

--- src/components/app/top/parts/LgtmImage.tsx
+++ src/components/app/top/parts/LgtmImage.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio } from "@radix-ui/themes";
+import { AspectRatio, Button } from "@radix-ui/themes";
 import Image from "next/image";
 import type { FC } from "react";
 import styles from "./LgtmImage.module.css";
@@ -6,22 +6,25 @@ import styles from "./LgtmImage.module.css";
 type Props = {
 	url: string;
 	alt: string;
+	onClick: () => void;
 };
 
-const LgtmImage: FC<Props> = ({ url, alt }) => {
+const LgtmImage: FC<Props> = ({ url, alt, onClick }) => {
 	return (
 		<AspectRatio ratio={4 / 3} className={styles["lgtm-image"]}>
-			<Image
-				src={url}
-				alt={alt}
-				style={{
-					objectFit: "cover",
-					width: "100%",
-					height: "100%",
-				}}
-				fill
-				sizes="100vw"
-			/>
+			<Button onClick={onClick} className={styles["button"]}>
+				<Image
+					src={url}
+					alt={alt}
+					style={{
+						objectFit: "cover",
+						width: "100%",
+						height: "100%",
+					}}
+					fill
+					sizes="100vw"
+				/>
+			</Button>
 		</AspectRatio>
 	);
 };
