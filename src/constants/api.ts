c04ea6bdd27c9a86a660ee80925c8b6c281e427f
br-to <barabara0224@gmail.com>
2024-11-03 16:38:11 +0900

    apiのパス修正

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,5 @@
 export const API_PATH = {
-	UPLOAD_IMAGE: "/api/upload-image",
+	UPLOAD_IMAGE: "/api/upload-image/",
+	UPLOAD_SEARCH_IMAGE: "/api/upload-search-image/",
+	SEARCH_IMAGES: "/api/search-images/",
 } as const;
